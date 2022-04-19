9c4da6477eb0a73b72902ab832c7d14bef305981
Sanjan611 <sanjandazz@gmail.com>
2022-04-19 21:45:40 +0000

    added models losses in separate scripts

---
+++ discriminator.py
@@ -0,0 +1,78 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Discriminator(nn.Module):
+  def __init__(self):
+     super(Discriminator, self).__init__()
+     self.conv_1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+      
+     self.conv_2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1)
+     self.conv_3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+     self.norm_1 = nn.BatchNorm2d(128)
+      
+     self.conv_4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1)
+     self.conv_5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+     self.norm_2 = nn.BatchNorm2d(256)
+    
+     self.conv_6 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+     self.norm_3 = nn.BatchNorm2d(256)
+    
+     self.conv_7 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=3, stride=1, padding=1)
+
+  def forward(self, x):
+    x = F.leaky_relu(self.conv_1(x))
+    x = F.leaky_relu(self.norm_1(self.conv_3(F.leaky_relu(self.conv_2(x)))), negative_slope=0.2)
+    x = F.leaky_relu(self.norm_2(self.conv_5(F.leaky_relu(self.conv_4(x)))), negative_slope=0.2)
+    x = F.leaky_relu(self.norm_3(self.conv_6(x)), negative_slope=0.2)
+    x = self.conv_7(x)
+    x = sigmoid(x)
+    
+    return x
+
+########################### LOSS #####################################
+
+from torchvision import models
+from torch.nn import BCELoss
+
+class DiscriminatorLoss(torch.nn.Module):
+  def __init__(self):
+      super(DiscriminatorLoss, self).__init__()
+      self.bce_loss = BCELoss()
+
+  def forward(self, discriminator_output_of_cartoon_input,
+              discriminator_output_of_cartoon_smoothed_input,
+              discriminator_output_of_generated_image_input,
+              epoch,
+              write_to_tensorboard=False):
+
+    return self._adversarial_loss(discriminator_output_of_cartoon_input,
+                     discriminator_output_of_cartoon_smoothed_input,
+                     discriminator_output_of_generated_image_input,
+                     epoch,
+                     write_to_tensorboard)
+
+  def _adversarial_loss(self, discriminator_output_of_cartoon_input,
+                     discriminator_output_of_cartoon_smoothed_input,
+                     discriminator_output_of_generated_image_input,
+                     epoch,
+                     write_to_tensorboard):
+
+    # define ones and zeros here instead within __init__ due to have same shape as input
+    # due to testing different batch_sizes, sometimes the "last batch" has < batch_size elements
+    actual_batch_size = discriminator_output_of_cartoon_input.size()[0]
+    zeros = torch.zeros([actual_batch_size, 1, 64, 64]).to(device)
+    ones = torch.ones([actual_batch_size, 1, 64, 64]).to(device)
+
+    d_loss_cartoon = self.bce_loss(discriminator_output_of_cartoon_input, ones)
+    d_loss_cartoon_smoothed = self.bce_loss(discriminator_output_of_cartoon_smoothed_input, zeros)
+    d_loss_generated_input = self.bce_loss(discriminator_output_of_generated_image_input, zeros)
+
+    d_loss = d_loss_cartoon + d_loss_cartoon_smoothed + d_loss_generated_input
+
+    if write_to_tensorboard:
+      writer.add_scalar('d_loss_cartoon', d_loss_cartoon,epoch)
+      writer.add_scalar('d_loss_cartoon_smoothed', d_loss_cartoon_smoothed, epoch)
+      writer.add_scalar('d_loss_generated_input', d_loss_generated_input, epoch)
+      writer.add_scalar('d_loss', d_loss, epoch)
+
+    return d_loss
---
+++ generator.py
@@ -0,0 +1,108 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import sigmoid
+
+class ResidualBlock(nn.Module):
+  def __init__(self):
+    super(ResidualBlock, self).__init__()
+    self.conv_1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+    self.conv_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+    self.norm_1 = nn.BatchNorm2d(256)
+    self.norm_2 = nn.BatchNorm2d(256)
+
+  def forward(self, x):
+    output = self.norm_2(self.conv_2(F.relu(self.norm_1(self.conv_1(x)))))
+    return output + x #ES
+
+class Generator(nn.Module):
+    def __init__(self):
+      super(Generator, self).__init__()
+      self.conv_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=1, padding=3)
+      self.norm_1 = nn.BatchNorm2d(64)
+      
+      # down-convolution #
+      self.conv_2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
+      self.conv_3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+      self.norm_2 = nn.BatchNorm2d(128)
+      
+      self.conv_4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1)
+      self.conv_5 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+      self.norm_3 = nn.BatchNorm2d(256)
+      
+      # residual blocks #
+      residualBlocks = []
+      for l in range(8):
+        residualBlocks.append(ResidualBlock())
+      self.res = nn.Sequential(*residualBlocks)
+      
+      # up-convolution #
+      self.conv_6 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=2, padding=1, output_padding=1)
+      self.conv_7 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+      self.norm_4 = nn.BatchNorm2d(128)
+
+      self.conv_8 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1)
+      self.conv_9 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
+      self.norm_5 = nn.BatchNorm2d(64)
+      
+      self.conv_10 = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=7, stride=1, padding=3)
+
+    def forward(self, x):
+      x = F.relu(self.norm_1(self.conv_1(x)))
+      
+      x = F.relu(self.norm_2(self.conv_3(self.conv_2(x))))
+      x = F.relu(self.norm_3(self.conv_5(self.conv_4(x))))
+      
+      x = self.res(x)
+      x = F.relu(self.norm_4(self.conv_7(self.conv_6(x))))
+      x = F.relu(self.norm_5(self.conv_9(self.conv_8(x))))
+
+      x = self.conv_10(x)
+
+      x = sigmoid(x)
+
+      return x
+
+
+############################### LOSS ##########################
+
+from torchvision import models
+from torch.nn import BCELoss
+
+class GeneratorLoss(torch.nn.Module):
+  def __init__(self):
+      super(GeneratorLoss, self).__init__()
+      self.w = 0.000005
+      self.bce_loss = BCELoss()
+      self.feature_extractor = vgg16.features[:24]
+      for param in self.feature_extractor.parameters():
+        param.require_grad = False
+
+  def forward(self, discriminator_output_of_generated_image_input,
+              generator_input,
+              generator_output,
+              epoch,
+              is_init_phase=False,
+              write_to_tensorboard=False):
+    if is_init_phase:
+      g_content_loss = self._content_loss(generator_input, generator_output)
+      g_adversarial_loss = 0.0
+      g_loss = g_content_loss
+    else:
+      g_adversarial_loss = self._adversarial_loss_generator_part_only(discriminator_output_of_generated_image_input)
+      g_content_loss = self._content_loss(generator_input, generator_output)
+      g_loss = g_adversarial_loss + self.w * g_content_loss
+
+    if write_to_tensorboard:
+      writer.add_scalar('g_adversarial_loss', g_adversarial_loss, epoch)
+      writer.add_scalar('g_content_loss', g_content_loss, epoch)
+      writer.add_scalar('g_loss', g_loss, epoch)
+
+    return g_loss
+
+  def _adversarial_loss_generator_part_only(self, discriminator_output_of_generated_image_input):
+    actual_batch_size = discriminator_output_of_generated_image_input.size()[0]
+    ones = torch.ones([actual_batch_size, 1, 64, 64]).to(device)
+    return self.bce_loss(discriminator_output_of_generated_image_input, ones)
+
+  def _content_loss(self, generator_input, generator_output):
+    return (self.feature_extractor(generator_output) - self.feature_extractor(generator_input)).norm(p=1)
